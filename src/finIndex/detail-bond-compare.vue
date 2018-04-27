04900e2664f3bd0937b259af8f60ede04a47ed66
jacon.xue <jacon.xue@sumscope.com>
2018-04-27 19:09:26 +0800

    债券比对UI

--- src/finIndex/detail-bond-compare.vue
+++ src/finIndex/detail-bond-compare.vue
@@ -1,34 +1,152 @@
 <template>
 <div class="wxc-demo">
-  <scroller class="scroller">
-     <text>债券对比页</text>
-  </scroller>
+
+  <div class="flex-column container">
+  
+    <div style="flex:1">
+      <div class="flex-row">
+        <div class="flex-column" v-for="(b,index) in bonds">
+          <input style="flex:1"  type="text" placeholder="输入需比较的词条..." class="input input-split" />
+          <text class="iconfont icon-search" @click="removeInput(index-1)" v-if="index!=0">&#xe6f3;</text>
+        </div>
+      </div>
+    </div>
+  
+    <text class="iconfont icon-search" @click="addBondCompare">&#xe6e0;</text>
+    
+  </div>  
+ 
+  
+    <scroller class="scroller" scroll-direction="horizontal">
+      <div class="flex-row" >
+        <div class="flex-column row-height">
+          <text class="w200 prop_name">属性/债券</text>
+          <text class="w250 prop_name">债券对比页2</text>
+          <text class="w250 prop_name">债券对比页3</text>
+          <text class="w250 prop_name">债券对比页4</text>
+          <text class="w250 prop_name">债券对比页5</text>
+        </div>
+        <scroller scroll-direction="vertical">
+          <div class="flex-column row-height" v-for="(item,index) in bondValues">
+            <text class="w200 prop_name">{{item.prop}}</text>
+            <text class="w250 prop_value">{{item.v1}}</text>
+            <text class="w250 prop_value">{{item.v2}}</text>
+            <text class="w250 prop_value">{{item.v3}}</text>
+            <text class="w250 prop_value">{{item.v4}}</text>
+          </div>
+        </scroller>
+      </div>   
+    </scroller>
+  
+
 </div>
 </template>
+<style scoped src='../css/sumslack.css' />
 <style scoped>
-  .wxc-demo {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    background-color: #ffffff;
-  }
-  
+.container {
+  margin-top:30px;
+  padding-left:10px;
+  padding-right: 30px;
+}
+.icon-search {
+  font-size: 40px;
+  margin-left:20px;
+}    
+.input-split {
+  margin-bottom: 5px;
+}
+.row-height {
+  height:50px;
+}
+.w200 {
+  width: 180px;
+}
+.w250 {
+  width: 250px;
+}
+.scroller {
+  flex:1;
+  margin-top:20px;
+  padding: 8px;
+}
+.prop_name {
+  color: #8F9598;
+  padding-right:20px;
+  padding-left: 10px;
+  font-size:25px;
+}
+.prop_value{
+  color: #FFEBC8;
+  font-size:25px;
+  padding-right: 10px;
+}
 </style>
 <script>
   import { WxcButton, WxcCell } from 'weex-ui';
   const Sumslack = require("../sumslack/js/sumslack.js");
+  import util from './util';
   module.exports = {
     components: { WxcButton, WxcCell  },
     data: () => ({
-
+      bonds:["k1","k2"],
+      bondValues:[
+        {prop:"简称",v1:"2015年贵阳",v2:"2015年贵阳",v3:"2015年贵阳",v4:"2015年贵阳"},
+        {prop:"债项评级",v1:"AA",v2:"AAA",v3:"AA",v4:"AAA"},
+        {prop:"主体评级",v1:"AAA",v2:"AAA",v3:"AAA",v4:"AAA"},
+        {prop:"债券类型",v1:"地方企业债 | 城投债",v2:"地方企业债 | 城投债",v3:"地方企业债 | 城投债",v4:"地方企业债 | 城投债"},
+        {prop:"发行规模",v1:"15.00亿元",v2:"15.00亿元",v3:"15.00亿元",v4:"115.00亿元"},
+        {prop:"债券期限",v1:"",v2:"",v3:"",v4:""},
+        {prop:"剩余期限",v1:"",v2:"",v3:"",v4:""},
+        {prop:"含权类型",v1:"",v2:"",v3:"",v4:""},
+        {prop:"还本方式",v1:"",v2:"",v3:"",v4:""},
+        {prop:"主承销商",v1:"",v2:"",v3:"",v4:""},
+        {prop:"承销团",v1:"",v2:"",v3:"",v4:""},
+        {prop:"担保方式",v1:"",v2:"",v3:"",v4:""},
+        {prop:"担保人",v1:"",v2:"",v3:"",v4:""},
+        {prop:"利率方式",v1:"固定利率",v2:"固定利率",v3:"固定利率",v4:"固定利率"},
+        {prop:"票面利率",v1:"4.17%",v2:"4.17%",v3:"4.17%",v4:"4.17%"},
+        {prop:"发行收益",v1:"4.17%",v2:"4.17%",v3:"4.17%",v4:"4.27%"},
+        {prop:"付息频率",v1:"年度",v2:"年度",v3:"年度",v4:"年度"},
+        {prop:"计息频率",v1:"年度",v2:"年度",v3:"年度",v4:"年度"},
+        {prop:"发行人",v1:"国有企业",v2:"国有企业",v3:"国有企业",v4:"国有企业"},
+        {prop:"发行机构",v1:"贵州贵安建设集团有限公司",v2:"贵州贵安建设集团有限公司",v3:"贵州贵安建设集团有限公司",v4:"贵州贵安建设集团有限公司"},
+        {prop:"起息日",v1:"20151028",v2:"20151028",v3:"20151028",v4:"20151028"},
+        {prop:"上市日",v1:"20151110",v2:"20151110",v3:"20151110",v4:"20151110"},
+        {prop:"下次付息日",v1:"20181028",v2:"20181028",v3:"20181028",v4:"20181028"},
+        {prop:"行权日",v1:"--",v2:"--",v3:"--",v4:"--"},
+        {prop:"下市日",v1:"20221027",v2:"20221027",v3:"20221027",v4:"20221027"},
+        {prop:"到期日",v1:"20221028",v2:"20221028",v3:"20221028",v4:"20221028"}
+      ]
     }),
     computed: {
       
     },
+    created() {
+      util.initIconFont();
+      Sumslack.init("债券比对",[{"title":"刷新","href":"javascript:refreshPage"}],function(){
+          Sumslack.addGlobalEventListener("refreshPage",function(){
+              Sumslack.refresh();
+          });
+      });
+    },
+    watch: {
+      bonds:function(val){
+        Sumslack.toast("bonds发生变化，执行比较");
+      }
+    },
     methods: {
-      
+      addBondCompare(){
+        if(this.bonds.length>5){
+          Sumslack.toast("债券比对最多只允许6只~");
+          return;
+        }
+        this.bonds.push("");
+      },
+      removeInput(index){
+        this.bonds = this.bonds.filter(function (item,_index) {
+          return index!=_index;
+        })
+      }
     }
   };
 </script>
--- src/finIndex/detail-bond.vue
+++ src/finIndex/detail-bond.vue
@@ -1,34 +1,211 @@
 <template>
 <div class="wxc-demo">
-  <scroller class="scroller">
-     <text>债券详情页</text>
-  </scroller>
+  <div class="flex-column search-container">
+    <div style="flex:1">
+      <input type="text" placeholder="输入词条..." class="input" @click="goSearchResult" />
+    </div>
+    <text class="iconfont icon-search" @click="gotoBondCompare">&#xe6e0;</text>
+    <text class="iconfont-selected icon-search" @click="favIt" v-if="isFav">&#xe6f4;</text>
+    <text class="iconfont icon-search" @click="favIt" v-else>&#xe6f5;</text>
+  </div>  
+  
+  <list class="list">
+    <cell class="cell">
+      <div class="panel">
+        <text class="panel-header">基本信息</text>
+        <div class="panel-body">
+          <div class="flex-column list-item flex-between" v-for="b in bond.base">
+            <text class="prop_name">{{b.propName}}：</text>
+            <text class="prop_value">{{b.propValue}}</text>
+          </div> 
+        </div>
+      </div>
+    </cell>
+
+    <cell class="cell">
+      <div class="panel">
+        <text class="panel-header">利率信息</text>
+        <div class="panel-body">
+          <div class="flex-column list-item flex-between" v-for="b in bond.rate">
+            <text class="prop_name">{{b.propName}}：</text>
+            <text class="prop_value">{{b.propValue}}</text>
+          </div> 
+        </div>
+      </div>
+    </cell>
+
+    <cell class="cell">
+      <div class="panel">
+        <text class="panel-header">流通市场信息</text>
+        <div class="panel-body">
+          <div class="flex-row list-item">
+            <div class="flex-column flex-between">
+              <text class="prop_name textcenter" style="flex:1">发行市场</text>
+              <text class="prop_name w4 textcenter">代码</text>
+              <text class="prop_name w4 textcenter">简称</text>
+              <text class="prop_name w100 textcenter">质押比</text>
+            </div>
+            <div class="flex-column flex-between rowspan" v-for="b in bond.market">
+              <text class="prop_value textcenter" style="flex:1">{{b.c1}}</text>
+              <text class="prop_value w4 textcenter">{{b.c2}}</text>
+              <text class="prop_value w4 textcenter">{{b.c3}}</text>
+              <text class="prop_value textcenter w100">{{b.c4}}</text>
+            </div>
+          </div> 
+        </div>
+      </div>
+    </cell>
+
+    <cell class="cell">
+      <div class="panel">
+        <text class="panel-header">债券日历</text>
+        <div class="panel-body">
+          <text class="prop_value">略</text>
+        </div>
+      </div>
+    </cell>  
+
+    <cell class="cell">
+      <div class="flex-column" style="margin-top:20px;margin-bottom:20px;">
+        <text class="prop_name">发行人</text>
+        <text class="prop_value">贵州贵安建设集团有限公司 国有企业</text> 
+      </div>
+    </cell>  
+
+    <cell class="cell">
+      <div class="panel">
+        <text class="panel-header">债项历史评级</text>
+        <div class="panel-body">
+          <text class="prop_value">略</text>
+        </div>
+      </div>
+    </cell>     
+
+    <cell class="cell">
+      <div class="panel">
+        <text class="panel-header">发行人所有债券(流通中)</text>
+        <div class="panel-body">
+          <text class="prop_value">略</text>
+        </div>
+      </div>
+    </cell> 
+
+    <cell class="cell">
+      <div class="panel">
+        <text class="panel-header">发行人主体历史评级</text>
+        <div class="panel-body">
+          <text class="prop_value">略</text>
+        </div>
+      </div>
+    </cell> 
+
+  </list>
+ 
 </div>
 </template>
+<style scoped src='../css/sumslack.css' />
 <style scoped>
-  .wxc-demo {
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    background-color: #ffffff;
-  }
-  
+.search-container {
+  margin-top:30px;
+  padding-left:10px;
+  padding-right: 30px;
+}
+.icon-search {
+  font-size: 50px;
+  margin-left:20px;
+}  
+.list-item {
+  margin-top: 8px;
+  margin-bottom: 8px;
+  margin-left: 8px;
+  margin-right: 8px;
+}
+.prop_name {
+  color: #8F9598;
+  padding-right:20px;
+  padding-left: 10px;
+  font-size:25px;
+}
+.rowspan {
+  margin-top:8px;
+  margin-bottom: 8px;
+}
+.prop_value{
+  color: #FFEBC8;
+  font-size:25px;
+  padding-right: 10px;
+}
+.w4 {
+  width: 172px;
+}
+.w100 {
+  width: 110px;
+}
 </style>
 <script>
   import { WxcButton, WxcCell } from 'weex-ui';
   const Sumslack = require("../sumslack/js/sumslack.js");
+
+  import util from './util';
+
   module.exports = {
     components: { WxcButton, WxcCell  },
     data: () => ({
-
+      bondKey:"",
+      isFav:true,
+      bond:{
+        base:[
+          {propName:"债券简称",propValue:"xxx"},
+          {propName:"债券全称",propValue:"2015年贵阳公路桥梁工程有限公司公司债券"},
+          {propName:"主体评级/债项评级",propValue:"AA/AAA"},
+          {propName:"债券类型",propValue:"地方企业债 | 城投债"},
+          {propName:"债券期限",propValue:"7.0000Y"},
+          {propName:"剩余期限",propValue:"4.50Y"},
+          {propName:"含权类型",propValue:"普通债券"},
+          {propName:"还本方式",propValue:"提前还本"},
+          {propName:"主承销商",propValue:"中信建投证券股份有限公司 "},
+          {propName:"承销团",propValue:"东海证券股份有限公司  国信证券股份有限公司 "},
+          {propName:"担保方式	",propValue:"保证担保"},
+          {propName:"担保人	",propValue:"中债信用增进投资股份有限公司"}
+        ],
+        rate:[
+          {propName:"利率方式",propValue:"固定利率"},
+          {propName:"票面利率",propValue:"4.17%"},
+          {propName:"发行收益	",propValue:"4.17%"},
+          {propName:"发行价格	",propValue:"100.00元"},
+          {propName:"付息频率	",propValue:"年度"},
+          {propName:"计息频率	",propValue:"年度"}
+        ],
+        market:[
+          {c1:"银行间债券市场",c2:"1580243",c3:"15贵阳路桥债",c4:"--"},
+          {c1:"上海证券交易所",c2:"127282.SH",c3:"15贵路桥",c4:"0.5300"}
+        ]
+      }
     }),
     computed: {
       
     },
+    created() {
+      util.initIconFont();
+      Sumslack.init("xxx债券",[{"title":"刷新","href":"javascript:refreshPage"}],function(){
+          Sumslack.addGlobalEventListener("refreshPage",function(){
+              Sumslack.refresh();
+          });
+      });
+    },
     methods: {
-      
+      goSearchResult(){
+
+      },
+      favIt(){
+        this.isFav = !this.isFav;
+        if(this.isFav){
+          Sumslack.toast("收藏成功!");
+        }
+      },
+      gotoBondCompare(){
+        Sumslack.navigateTo("fin.detail.bond.compare",{bondKey:"xxx"});
+      }
     }
   };
 </script>
